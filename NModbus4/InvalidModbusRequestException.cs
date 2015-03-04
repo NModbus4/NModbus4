07f1d1487cf8801369fac3f7551b9b46f3d2ca83
Edward Brey <edward.brey@toolsay.com>
2015-03-04 10:39:36 -0600

    added InvalidModbusRequestException.GetObjectData

--- NModbus4/InvalidModbusRequestException.cs
+++ NModbus4/InvalidModbusRequestException.cs
@@ -55,6 +55,15 @@
         protected InvalidModbusRequestException(SerializationInfo info, StreamingContext context)
             : base(info, context) { }
 
+        /// <summary>Sets the <see cref="SerializationInfo" /> object with the Modbus exception code and additional exception information.</summary>
+        /// <param name="info">The <see cref="SerializationInfo" /> that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="context">The <see cref="StreamingContext" /> that contains contextual information about the source or destination.</param>
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+            info.AddValue("ExceptionCode", this._exceptionCode, typeof(byte));
+        }
+
         /// <summary>
         ///     Gets the Modbus exception code to provide to the slave.
         /// </summary>
