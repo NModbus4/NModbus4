d6f47948210ceadaf7ce2fbbfbd3e1798a529dd7
Dmitry Turin <turin.dmytro@gmail.com>
2015-06-04 16:56:51 +0300

    Prepare for the next dev iteration

--- NModbus4/Properties/AssemblyInfo.cs
+++ NModbus4/Properties/AssemblyInfo.cs
@@ -1,20 +1,26 @@
 using System;
 using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("NModbus4")]
 [assembly: AssemblyProduct("NModbus4")]
-[assembly: AssemblyCompany("Maxwe11")]
-[assembly: AssemblyCopyright("Licensed under MIT License.")]
-[assembly: AssemblyDescription("NModbus is a C# implementation of the Modbus protocol. " +
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright © 2006 Scott Alexander, 2015 Dmitry Turin")]
+[assembly: AssemblyDescription("NModbus4 is a C# implementation of the Modbus protocol. " +
            "Provides connectivity to Modbus slave compatible devices and applications. " +
-           "Supports serial ASCII, serial RTU, TCP, and UDP protocols. "+
+           "Supports ASCII, RTU, TCP, and UDP protocols. " +
            "NModbus4 it's a fork of NModbus(https://code.google.com/p/nmodbus)")]
 
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(false)]
-[assembly: Guid("95B2AE1E-E0DC-4306-8431-D81ED10A2D5D")]
-[assembly: AssemblyVersion("2.0.*")]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyInformationalVersion("3.0.0-dev")]
 
 #if !SIGNED
 [assembly: InternalsVisibleTo(@"Modbus.UnitTests")]
