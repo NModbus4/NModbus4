8fb59429ea43eafd4e34dd6c2d80d9ce54e9db8f
Dmitry Turin <turin.dmytro@gmail.com>
2015-08-19 18:23:47 +0300

    Initial port of TestDriver

---
+++ NModbus4.TestDriver/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace NModbus4.TestDriver
+{
+    using System;
+    using System.Net.Sockets;
+    using Modbus.Device;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (TcpClient client = new TcpClient("127.0.0.1", 502))
+                {
+                    client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
+
+                    var master = ModbusIpMaster.CreateIp(client);
+                    // read five input values
+                    ushort startAddress = 100;
+                    ushort numInputs = 5;
+                    bool[] inputs = master.ReadInputs(startAddress, numInputs);
+
+                    for (int i = 0; i < numInputs; i++)
+                        Console.WriteLine("Input {0}={1}", startAddress + i, inputs[i] ? 1 : 0);
+
+                    while (true)
+                    {
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
